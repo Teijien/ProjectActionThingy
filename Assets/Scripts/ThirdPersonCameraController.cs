99fc4733e4f9b51f6a7bcf9cc6ef7fa2cd1a229d
Richard Torres <riatorre@ucsc.edu>
2020-08-22 03:13:11 -0700

    Reorganized Again + Implemented Camera using Cinemachine
    Fixed organization of Scripts folder such that Unity recognizes its location. Additionally, reworked Camera control + movement to use Cinemachine. This is a package that comes with Unity that allows us to have a production level camera control. Reworked character movement such that character will now face the direction it last travelled in while allowing movement to be relative to the camera's direction.

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float Speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized;
+
+        if(playerMovement.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * Speed * Time.deltaTime);
+        }
+    }
+}
