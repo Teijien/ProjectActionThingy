d918b04f6e7f5670ac13717cba4198ddbdafaa6b
Richard Torres <riatorre@ucsc.edu>
2020-08-23 01:54:55 -0700

    Added Basic animation blending with movement + PS4 Controller Input
    Added a temporary animation controller that blends the idle, walk, and run animations for the character and tied the rate of blend to the character's movement. Additionally, I have updated the Input Manager to allow for PS4 controller input. Should work with PS4 controllers connected with bluetooth or wireless through a program called DS4Windows. Not sure if it works without DS4Windows running

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -6,6 +6,9 @@ public class ThirdPersonCharacterController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Animator animator;
+
+    float movement = 0f;
 
     public float Speed = 6f;
     public float turnSmoothTime = 0.1f;
@@ -20,12 +23,17 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
         if(playerMovement.magnitude >= 0.1f)
         {
+            movement += Speed * Time.deltaTime;
             float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * Speed * Time.deltaTime);
+        }else{
+            movement -= Speed * Time.deltaTime;
         }
+        movement = Mathf.Clamp(movement, 0f, 1f);
+        animator.SetFloat("movementForward", movement);
     }
 }
