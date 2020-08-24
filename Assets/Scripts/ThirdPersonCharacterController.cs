08ecd8a5b8dd5f206194d77b5870795713575b06
Richard Torres <riatorre@ucsc.edu>
2020-08-24 13:05:31 -0700

    Added Jump
    Implemented Logic for Jumping & set up animation transition logic for future use.

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -9,31 +9,62 @@ public class ThirdPersonCharacterController : MonoBehaviour
     public Animator animator;
 
     float movement = 0f;
+    bool jump = false;
 
     public float Speed = 6f;
+    public float jumpForce = 2f;
+    public float gravity = 4f;
     public float turnSmoothTime = 0.1f;
+    float verticalVelocity;
     float turnSmoothVelocity;
+    Vector3 moveDir;
 
     // Update is called once per frame
     void Update()
     {
+        moveDir = Vector3.zero;
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
         Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized;
 
+        // Movement Logic
         if(playerMovement.magnitude >= 0.1f)
         {
-            movement += Speed * Time.deltaTime;
+            // Update player's location if the magnitude of the input is significant enough
             float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * Speed * Time.deltaTime);
-        }else{
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDir = moveDir.normalized;
+            movement += Speed * Time.deltaTime;
+
+        }
+        else
+        {
             movement -= Speed * Time.deltaTime;
         }
+
+        // Jump Logic
+        if(controller.isGrounded)
+        {
+            jump = false;
+            if(Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpForce;
+                jump = true;
+            }
+        }
+        else
+        {
+            jump = true;
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        moveDir.y = verticalVelocity;
+        controller.Move(moveDir * Speed * Time.deltaTime);
+
         movement = Mathf.Clamp(movement, 0f, 1f);
         animator.SetFloat("movementForward", movement);
+        animator.SetBool("Jump", jump);
     }
 }
