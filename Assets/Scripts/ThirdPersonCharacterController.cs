67ad45ef00718c755600db53f4c7f07da6521dc8
Richard Torres <riatorre@ucsc.edu>
2020-08-25 21:09:51 -0700

    Cleaning up movement controller

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class ThirdPersonCharacterController : MonoBehaviour
 {
+    // Component Inputs
     public CharacterController controller;
     public Transform cam;
     public Animator animator;
 
-    float movement = 0f;
-    bool jump = false;
-
+    // Input variables
     public float Speed = 6f;
+    public float turnSmoothTime = 0.1f;
     public float jumpForce = 2f;
     public float gravity = 4f;
-    public float turnSmoothTime = 0.1f;
-    float verticalVelocity;
+
+    // Private variables
+    float movement = 0f;
+    bool jump = false;
+    float yVelocity = 0f;
     float turnSmoothVelocity;
     Vector3 moveDir;
 
@@ -28,42 +31,46 @@ public class ThirdPersonCharacterController : MonoBehaviour
         Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized;
 
         // Movement Logic
+        
         if(playerMovement.magnitude >= 0.1f)
         {
             // Update player's location if the magnitude of the input is significant enough
-            float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float targetAngle = Mathf.Atan2(playerMovement.x, playerMovement.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // Find angle to move towards relative to camera direction
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); // Dampen Angle for character rotation
+            transform.rotation = Quaternion.Euler(0f, angle, 0f); // Rotate character (Mesh) towards direction of movement
 
-            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            moveDir = moveDir.normalized;
-            movement += Speed * Time.deltaTime;
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; //Convert angular direction to XYZ coordinate units to move towards
+            moveDir = moveDir.normalized; // Normalize units to make diagonal movement to be the same speed as movement on axis
+            movement += Speed * Time.deltaTime; // Change the value of the idle-walk-run blend control variable.
 
         }
         else
         {
-            movement -= Speed * Time.deltaTime;
+            movement -= Speed * Time.deltaTime; // Decay blend variable as no horizontal plane movement detected.
         }
 
         // Jump Logic
         if(controller.isGrounded)
         {
-            jump = false;
-            if(Input.GetButtonDown("Jump"))
+            jump = false; // Character is on the ground, set jump boolean to false
+            if(Input.GetButtonDown("Jump")) // Listen for "Jump" button to be pressed
             {
-                verticalVelocity = jumpForce;
-                jump = true;
+                yVelocity = jumpForce; // Update jump velocity
+                jump = true; // Toggle jump boolean for animation switch
             }
         }
         else
         {
+            // Character is not on the ground; character is in the air.
             jump = true;
-            verticalVelocity -= gravity * Time.deltaTime;
+            yVelocity -= gravity * Time.deltaTime; // Update vertical velocity for gravity.
         }
-        moveDir.y = verticalVelocity;
-        controller.Move(moveDir * Speed * Time.deltaTime);
+        moveDir.y = yVelocity; // Add the vertical velocity component to the overall movement
+        controller.Move(moveDir * Speed * Time.deltaTime); // Move the actual character
 
-        movement = Mathf.Clamp(movement, 0f, 1f);
+        movement = Mathf.Clamp(movement, 0f, 1f); // Clamp the value for animation blending.
+
+        // Store Values for animation blending and transitions
         animator.SetFloat("movementForward", movement);
         animator.SetBool("Jump", jump);
     }
